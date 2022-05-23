43f7d4a56e2f690a6f003aa5f4676093e0df6ea9
Matt De Binion <debinionm@gmail.com>
2022-05-23 14:51:45 -0700

    UI fixes and clean up

--- src/TicTacToeGame/Client.java
+++ src/TicTacToeGame/Client.java
@@ -11,6 +11,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 /**
@@ -49,6 +50,9 @@ public class Client extends Application {
         scene = new Scene(root);
         scene.getStylesheets().add(getClass().getResource("stylesheets/style.css").toExternalForm());
 
+        Image icon = new Image(getClass().getResourceAsStream("icon.png"));
+        stage.getIcons().add(icon);
+        stage.setResizable(false);
         stage.setTitle("Tic Tac Toe");
         stage.setScene(scene);
         stage.show();
